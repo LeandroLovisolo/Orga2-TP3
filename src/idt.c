145527ee31326c0ca9f3045125b7b25e465bdf99
Lautaro Petaccio <lausuper@gmail.com>
2013-05-28 21:25:11 -0300

    IDT completa

--- src/idt.c
+++ src/idt.c
@@ -10,12 +10,14 @@
 #include "idt.h"
 #include "i386.h"
 
+
+
 idt_entry idt[255] = { };
 
 idt_descriptor IDT_DESC = {
 	sizeof(idt) - 1,
 	(unsigned int) &idt
-};
+}; 	
 
 
 /*
@@ -40,8 +42,30 @@ idt_descriptor IDT_DESC = {
 	idt[numero].attr = (unsigned short) atrr; 	                                                                 \
 	idt[numero].offset_16_31 = (unsigned short) ((unsigned int)(&_isr ## numero) >> 16 & (unsigned int) 0xFFFF);
 
-//Segmento 8 en la GDT, 8*2^3 (3 shifts) = 64
+//Segmento 8 en la GDT, 1*2^3 (3 shifts) = 64
 //Atributos: P=1,DPL=00,0,D=1,Tipo de puerta=110,0000,0000 = 0x8E00
+#define GATE_TYPE 0x8E00
+
 void idt_inicializar() {
-	IDT_ENTRY(0, 64, 0x8E00);
+	IDT_ENTRY(0, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(1, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(2, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(3, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(4, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(5, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(6, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(7, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(8, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(9, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(10, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(11, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(12, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(13, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(14, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(15, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(16, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(17, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(18, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(19, KERNEL_CODE_SEGMENT, GATE_TYPE);
+	IDT_ENTRY(20, KERNEL_CODE_SEGMENT, GATE_TYPE);
 }
