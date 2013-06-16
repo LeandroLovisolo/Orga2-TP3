e9ba20b3b82d822596be5b6e7d339b6fc1716841
Lautaro Petaccio <lausuper@gmail.com>
2013-06-16 12:47:07 -0300

    Funcion asignar memoria para mappear cuando las tareas pidan

--- src/isr.h
+++ src/isr.h
@@ -7,6 +7,8 @@
 
 #ifndef __ISR_H__
 #define __ISR_H__
+#include "sched.h"
+#include "mmu.h"
 
 void _isr0();
 void _isr1();
@@ -35,6 +37,31 @@ void _isr128();
 void _isr144();
 void jmpToTask(unsigned short tarea);
 
+unsigned int ultimaDirfisica = 0x164000;
+unsigned short cantPaginas[5];
+
+cantPaginas[0] = 0;
+cantPaginas[1] = 0;
+cantPaginas[2] = 0;
+cantPaginas[3] = 0;
+cantPaginas[4] = 0;
+/*
+La idea de asignar memoria es que si piden una dirección virtual entre las del enunciado
+le demos una página de memoria fisica. Cada tarea puede tener a lo sumo 5 páginas
+No es necesario llevar el rastro de a que tarea se le asigna cual dirección fisica o algo similar
+Ya que no se necesita liberar memoria y las direcciones que ya se mapean no vuelven a tirar PF*/
+char asignarMemoria(unsigned int direccion) {
+	unsigned short tarea = tareaActiva()-10; //Me da el valor del indice de la tarea
+	if((direccion >= 0x003D0000) && (direccion <= 123123) && (cantPaginas[tarea] <= 5)) {
+		cantPaginas[tarea]++;
+		mmu_mappear_pagina(direccion, ultimaDirfisica, directorioDeTareas[tarea], 0, 1);
+		ultimaDirfisica += 4096; //Ver si hay que sumarle uno también
+		return 1;
+	}
+	else {
+		return 0;
+	}
+} 
 
 
 
